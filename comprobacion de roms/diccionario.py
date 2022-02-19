39cd78dd75d3eee2e143500caea44d2c263d617a
eloyrj <48245099+eloyrj@users.noreply.github.com>
2022-02-19 14:42:34 +0100

    Update diccionario.py

--- comprobacion de roms/diccionario.py
+++ comprobacion de roms/diccionario.py
@@ -1,9 +1,9 @@
 
 
-with open('pDic.txt', 'r') as archivo:
+with open('diccionario.txt', 'r',encoding="utf-8") as archivo:
     separador = ':'
     data = {}
-    
+
     for linea in archivo:
         value, key = linea.split(separador)
         data[key.strip()] = value.strip()
