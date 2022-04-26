26d24829217f67876e48dffecab8809d82f95eb9
eloyrj <eloy.rodriguezj@edu.uah.es>
2022-04-26 22:05:25 +0200

    cuadros

--- comprobacion de roms/comprobarPokemons.py
+++ comprobacion de roms/comprobarPokemons.py
@@ -1,19 +1,22 @@
 
 from cProfile import label
+from doctest import master
 from math import fabs
 from tkinter import *
 from tkinter import filedialog
 from PIL import ImageTk, Image
 import os
-print(os.path.abspath(os.getcwd()))
+
 rutaGeneral = os.path.abspath(os.getcwd())
 
 rutaLog =""
 rutaLectura =""
-nPokemons = 6
+nPokemons = 8
+mo = ["Golpe Roca", "corte", "vuelo", "surf", "fuerza", "despejar", "cascada", "treparrocas"]
 
 def abrirLog():
-    archivo = filedialog.askopenfilename()
+    archivo = filedialog.askopenfilename(initialdir="/home/corte/Descargas/no comprobados")
+    
     global rutaLog
     rutaLog = archivo
     
@@ -30,7 +33,6 @@ def cargarDiccionario(ruta):
         for linea in archivo:
             value, key = linea.split(separador)
             data[key.strip()] = value.strip()
-
     return data
 
 def cargarDiccionarios():
@@ -38,7 +40,7 @@ def cargarDiccionarios():
 
 def compruebaMT(ataque):
     f2 = open(rutaLog,"r",encoding='utf-8')
-    lines = f2.readlines()[2504:2595]
+    lines = f2.readlines()[2514:2607]
     for line in lines:
         
         mt = line.split(" ")[0]
@@ -47,39 +49,71 @@ def compruebaMT(ataque):
         
     return False,mt
 
+
+
+
 def generarArchivoConData():
-    nivellb.config(text=" ")
-    lb1.config(text=" ")
-    PokemonAparece = False
-    ataquesCorrectos = False
-    naturalezaCorrecta = False
-    HabilidadCorrecta = False
+    c = cargarDiccionarios() #Carga de diccionarios.
+    diccionarioAtaques=c[0]
+    diccionarioNaturaleza=c[1]
+    diccionarioHabilidades=c[2]
+
+    nivellb.config(text=" ")# fijamos label de nivel a vacio
+    lb1.config(text=" ")    # Fijamos label a vacio
+    
+    PokemonAparece = False # Variable de si aparece el pokemon en el archivo 
+    ataquesCorrectos = False # variable de si todos los ataques estan correctos
+    naturalezaCorrecta = False # variable para ver si la naturaleza es correcta
+    HabilidadCorrecta = False # variable para ver si la habilidad es correcta
     
     
     f1 = open(rutaLectura)
-    salida = open(rutaGeneral + "/salida.txt","w",encoding="utf-8")
+    salida = open(rutaGeneral + "/salida.txt","w",encoding="utf-8") #creamos la variable salida para escrir en el documento
 
-    f2 = open(rutaLog,"r",encoding='utf-8')
-    with f2 as myfile:
-        total_lines = sum(1 for line in myfile)
-    f2.close()
-
-    repetidas = 0
+    
     encontrado = False
     bajo = 0
     alto = 10
-    for p in range(nPokemons):
+    for p in range(nPokemons): # Bucle principal para comprobar todos los pokemons del equipo nPokemons nos dice ese numero, es una constante.
+        
         #-------------------------------------------------------------------------------------------------------------------------
+        f1 = open(rutaLectura) # Lectura del archivo de Pokemon showdown
+        pala = f1.readlines()[bajo:alto]
+
+        if pala[3].split(" ")[0].strip() == "Shiny:": #comprobacion de si hay iv en el archivo
+            l = 5
+            l2 =6
+        else:
+            l = 4
+            l2 = 5
+        if pala[l2].split(" ")[0].strip() == "IVs:": #comprobacion de si hay iv en el archivo
+            longi = 10
+        else:
+            longi = 9
+        if pala[3].split(" ")[0].strip() == "Shiny:": #comprobacion de si hay iv en el archivo
+            longi =longi+1
+            
+        bajo = bajo 
+        alto = bajo + longi
+        
+        
+        f1.close()
         f1 = open(rutaLectura)
         pala = f1.readlines()[bajo:alto]
+
+        
         sete = pala[0].split(" ")
-        if len(sete)>3:
-            pokemon = sete[1][1:(len(sete[1])-1)]
+        if sete[1] != '(F)' and sete[1] != '(M)' and sete[1][0]=="(":
+            pokemon = sete[1][1:(len(sete[1])-1)] #pokemon si el pokemon tiene nombre
         else:
-            pokemon = sete[0]
-            
+            pokemon = sete[0] #pokemon si no tiene nombre"""
+        
+        if pokemon.upper() =="DEOXYS-ATTACK":
+            pokemon="Deoxys"
+
+        
         evoluciones = []
-        with open(rutaGeneral + "/evolucionesOrden.txt", 'r') as archivo:
+        with open(rutaGeneral + "/evolucionesOrden.txt", 'r') as archivo:  #lecturas de las evoluciones y cargadalas a evoluciones
         
             for linea in archivo:
                 if linea.find(pokemon)>=0:
@@ -88,9 +122,9 @@ def generarArchivoConData():
                         if i != (len(value)-1):
                             evoluciones += [value[i]]
         
-        nivel = pala[2].split(" ")[1]
+        nivel = pala[2].split(" ")[1] # lectura del nivel del pokemon
         nivel = int(nivel)
-        if nivel <= 50:
+        if nivel <= 50: #comprovacion del nivel del pokemon 
             nivelValido = True
         else:
             nivelValido = False
@@ -109,17 +143,24 @@ def generarArchivoConData():
             else:
                 habilidadIngles = habilidadIngles+j
             
-        habilidad = diccionarioHabilidades.get(habilidadIngles.upper())
-
-        naturalezaIngles = pala[4].split(" ")[0]
-        naturaleza = diccionarioNaturaleza.get(naturalezaIngles)
-        
-        if pala[5].split(" ")[0].strip() == "IVs:":
+        habilidad = diccionarioHabilidades.get(habilidadIngles.upper()) #fijacion de la habilidad ya traducida
+        if pala[3].split(" ")[0].strip() == "Shiny:": #comprobacion de si hay iv en el archivo
+            l = 5
+            l2 =6
+        else:
+            l = 4
+            l2 = 5
+        naturalezaIngles = pala[l].split(" ")[0] # lectura de naturaleza en ingles
+        naturaleza = diccionarioNaturaleza.get(naturalezaIngles) # naturaleza ya traducida
+        if pala[l2].split(" ")[0].strip() == "IVs:": #comprobacion de si hay iv en el archivo
             longi = 10
         else:
             longi = 9
+        if pala[3].split(" ")[0].strip() == "Shiny:": #comprobacion de si hay iv en el archivo
+            longi =longi+1
         
-       
+        
+        #----------- Lectura ataques------------------
 
         ataque1apoyo = pala[(longi-4)].split(" ")[1:len(pala[(longi-4)].split(" "))-2]
         ataque1Ingles = ""
@@ -128,7 +169,7 @@ def generarArchivoConData():
                 ataque1Ingles = ataque1Ingles+i+" "
             else:
                 ataque1Ingles = ataque1Ingles+i
-        ataque1 = diccionarioAtaques.get(ataque1Ingles)
+        ataque1 = diccionarioAtaques.get(ataque1Ingles) 
 
         ataque2apoyo = pala[(longi-3)].split(" ")[1:len(pala[(longi-3)].split(" "))-2]
         ataque2Ingles = ""
@@ -149,22 +190,24 @@ def generarArchivoConData():
         ataque3 = diccionarioAtaques.get(ataque3Ingles)
 
         ataque4apoyo = pala[(longi-1)].split(" ")[1:len(pala[(longi-1)].split(" "))-2]
+        
         ataque4Ingles = ""
         for i in ataque4apoyo:
             if i != ataque4apoyo[len(ataque4apoyo)-1]:
                 ataque4Ingles = ataque4Ingles+i+" "
             else:
                 ataque4Ingles = ataque4Ingles+i
+     
         ataque4 = diccionarioAtaques.get(ataque4Ingles)
-        
-       
+        #---------------fin lectura ataques-------------------------
+
         
         lbs[p].config(text=naturaleza)
             
         PosicionTexto = -1
         NumeroLinea = 0
         #------------------------------------------------------------------------------------------
-        PokemonAparece = False
+        PokemonAparece = False #fijamos todas las variables a no encontrado.
         ataquesCorrectos = False
         naturalezaCorrecta = False
         HabilidadCorrecta = False
@@ -173,23 +216,22 @@ def generarArchivoConData():
         ataque3Correctos = False
         ataque4Correctos = False
         
+
         
-        for h in evoluciones:
-            pokemon = h.upper()
+        
+        for h in evoluciones: # bucle de evoluciones
+            pokemon = h.upper()# fijamos el pokemon a la evolucion
             
-            print(pokemon)
-            print(ataque1)
-            print(ataque2)
-            print(ataque3)
-            print(ataque4)
+            #print(pokemon)
+            #print(ataque1)
+            #print(ataque2)
+            #print(ataque3)
+            #print(ataque4)
             
-            f2 = open(rutaLog,"r",encoding='utf-8')
-            
-            lines = f2.readlines()[1944:2501]
+            f2 = open(rutaLog,"r",encoding='utf-8')# Vemos si el pokemon aparece en el log
+            lines = f2.readlines()[1954:2513]
             encontrado= False
-        
             for line in lines:
-                
                 NumeroLinea = NumeroLinea + 1
                 PosicionTexto = line.find(pokemon)
                 if PosicionTexto >= 0:
@@ -199,12 +241,14 @@ def generarArchivoConData():
                     salida.write(" --> ")
                     salida.write(line)
                     PokemonAparece = True
-                
             f2.close()
-            if(encontrado):
+            
+            
+            if(encontrado):# si aparece
 
+                # ------------------------Comprobacion de si los ataques son correctos---------------------------------
                 f2 = open(rutaLog,"r",encoding='utf-8')
-                lines = f2.readlines()[520:1023]
+                lines = f2.readlines()[530:1024]
                 for line in lines:
                     NumeroLinea = NumeroLinea + 1
                     PosicionTexto = line.find(pokemon)
@@ -214,33 +258,40 @@ def generarArchivoConData():
                         salida.write(" --> ")
                         salida.write(line)
                         
-                        
+                        for i in mo: #comprobacion de mo's
+                            if i.upper() == ataque1.upper():
+                                ataque1Correctos = True
+                            if i.upper() == ataque2.upper():
+                                ataque2Correctos = True
+                            if i.upper() == ataque3.upper():
+                                ataque3Correctos = True
+                            if i.upper() == ataque4.upper():
+                                ataque4Correctos = True
                         
                         if line.find(ataque1)>=0: 
                             ataque1Correctos = True
-                        
                             
                         if line.find(ataque2)>=0 : 
                             ataque2Correctos = True
-                        
                             
                         if line.find(ataque3)>=0: 
                             ataque3Correctos = True
-                        
                             
                         if  line.find(ataque4)>=0:
                             ataque4Correctos = True
-                        
                             
                         if ataque1Correctos and ataque2Correctos and ataque3Correctos and ataque3Correctos and ataque4Correctos :
-                            print("los ataques estan bien "+ pokemon)
+                            #print("los ataques estan bien "+ pokemon)
                             ataquesCorrectos = True
                             
                 f2.close()
+                #--------------------------Fin comprobacion ataques-----------------------------
+                
+                
 
-
+                #-----------------------Comprobacion de Habilidad-----------------------------
                 f2 = open(rutaLog,"r",encoding="utf-8")
-                lines = f2.readlines()[3:495]
+                lines = f2.readlines()[1:496]
                 for line in lines:       
                     NumeroLinea = NumeroLinea + 1
                     PosicionTexto = line.find(pokemon)
@@ -250,26 +301,62 @@ def generarArchivoConData():
                         salida.write(" --> ")
                         salida.write(line)    
                         if(habilidad == None):
-                            print("habilidad no encontrada")
+                            #print("habilidad no encontrada")
                             return False
                         
-                        if habilidad ==   line.split("|")[9].upper().strip() or habilidad == line.split("|")[10].upper().strip():
-                            print( "la habilidad es correcta")
+                        if habilidad.upper() ==   line.split("|")[9].upper().strip() or habilidad.upper() == line.split("|")[10].upper().strip():
+                            #print( "la habilidad es correcta")
                             HabilidadCorrecta = True
-                        
-                            
                 salida.write( '\n' )
                 f2.close()
-                if(HabilidadCorrecta and ataquesCorrectos and PokemonAparece and nivelValido):
-                    break
+                #--------------------Fin comprobacion de habilidad--------------------------------
+                
+                if(HabilidadCorrecta and ataquesCorrectos and PokemonAparece and nivelValido):#comprobamos si todos los valores del pokemon son correctos
+                    break # si son corerctos paramos el bucle de evoluciones para dar paso a el siguiente pokemon.
+                
             
-        else:
+        else: # si el pokemon no aparece en el log.
             s =  "No se ha encontado a ningun "+pokemon
             salida.write(s)
             salida.write( '\n' )
             salida.write( '\n' )
         
-        if not ataquesCorrectos:
+        if not ataquesCorrectos: #comprobamos mt's y movimientos de tutuor.
+            f2 = open(rutaLog,"r",encoding='utf-8')
+            lines = f2.readlines()[2609:2647]
+            for line in lines:
+                NumeroLinea = NumeroLinea + 1
+                if line.find(ataque1)>=0: 
+                    ataque1Correctos = True
+                    print("Ataque ",ataque1," obtenida de tutor")
+                elif ataque1 == "Cometa Draco":
+                    ataque1Correctos = True
+
+                
+                if line.find(ataque2)>=0 : 
+                    ataque2Correctos = True
+                    print("Ataque ",ataque2," obtenida de tutor")
+                elif ataque2 == "Cometa Draco":
+                    ataque2Correctos = True
+
+                    
+                if line.find(ataque3)>=0: 
+                    ataque3Correctos = True
+                    print("Ataque ",ataque3," obtenida de tutor")
+                elif ataque3 == "Cometa Draco":
+                    ataque3Correctos = True
+
+                            
+                if  line.find(ataque4)>=0:
+                    ataque4Correctos = True
+                    print("Ataque ",ataque4," obtenida de tutor")
+                elif ataque4 == "Cometa Draco":
+                    ataque4Correctos = True
+                    
+                if ataque1Correctos and ataque2Correctos and ataque3Correctos and ataque3Correctos and ataque4Correctos :
+                    ataquesCorrectos = True
+            f2.close()
+            
             
             if compruebaMT(ataque1)[0] and not ataque1Correctos:
                 ataque1Correctos = True
@@ -288,20 +375,28 @@ def generarArchivoConData():
                 mtslb[p].config(text=compruebaMT(ataque4)[1])
             
             if ataque1Correctos and ataque2Correctos and ataque3Correctos and ataque3Correctos and ataque4Correctos :
-                print("los ataques estan bien "+ pokemon)
                 ataquesCorrectos = True
             
             
         
         bajo = bajo + longi+1
         alto = alto + longi+1
-        if (not HabilidadCorrecta or not ataquesCorrectos or not PokemonAparece or not nivelValido):
+        
+        
+        if (not HabilidadCorrecta or not ataquesCorrectos or not PokemonAparece or not nivelValido): #Impresion de errores por terminal.
             lb1.config(text="El Equipo NO Es Valido")
-            print(False)
+            print("Pokemon: "+pokemon)
+            print("habilidades: ",HabilidadCorrecta," Ataques: ", ataquesCorrectos," Pokemon: ", PokemonAparece, " Nivel: ", nivelValido)
+            if(not HabilidadCorrecta):
+                print("habilidad: ",habilidad," -> ",HabilidadCorrecta)
+            if(not ataquesCorrectos):
+                print("Ataque 1: ",ataque1," ->", ataque1Correctos)
+                print("Ataque 2: ",ataque2," ->", ataque2Correctos)
+                print("Ataque 3: ",ataque3," ->", ataque3Correctos)
+                print("Ataque 4: ",ataque4," ->", ataque4Correctos)
             return False
         
-        if(HabilidadCorrecta and ataquesCorrectos and PokemonAparece and nivelValido):
-            break
+        
         
                 
         
@@ -311,12 +406,9 @@ def generarArchivoConData():
     return True
 
 
+# Comienzo del programa
 
 
-c = cargarDiccionarios()
-diccionarioAtaques=c[0]
-diccionarioNaturaleza=c[1]
-diccionarioHabilidades=c[2]
 
 
 
